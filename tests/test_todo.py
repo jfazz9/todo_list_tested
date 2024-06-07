457241152aa2f864efa972e27cf93e808b681c38
jfazz9 <jftennis@gmail.com>
2024-06-07 17:45:52 +0100

    added additional tests, followed by functions

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -4,13 +4,26 @@ from todo_list.todo import TodoList
 class TestTodoList(unittest.TestCase):
     def test_add_task(self):
         todo = TodoList()
-        todo.add_task('Buy socks')
-        self.assertIn('Buy socks', todo.tasks)
+        todo.add_task('Buy stocks')
+        self.assertIn('Buy stocks', todo.tasks)
 
     def test_remove_task(self):
         todo = TodoList()
-        todo.add_task('Celery')
-        todo.remove_task('Celery')
-        self.assertNotIn('Celery',todo.tasks)
+        todo.add_task('meditate')
+        todo.remove_task('meditate')
+        self.assertNotIn('meditate',todo.tasks)
+
+    def test_list_tasks(self):
+        todo = TodoList()
+        todo.add_task('gym time')
+        todo.add_task('read')
+        todo.add_task('play sport')
+        self.assertEqual(['gym time', 'read', 'play sport'], todo.tasks)
     
-    
+    def test_completed_tasks(self):
+        todo = TodoList()
+        todo.add_task('study')
+        todo.add_task('code')
+        todo.completed_task('code')
+        self.assertIn('code', todo.completed)
+        self.assertNotIn('code', todo.tasks)
--- todo_list/todo.py
+++ todo_list/todo.py
@@ -2,8 +2,16 @@
 class TodoList():
     def __init__(self):
         self.tasks = []
+        self.completed = []
 
     def add_task(self, task):
         self.tasks.append(task)
+    
+    def remove_task(self,task):
+        self.tasks.remove(task)
 
-
+    def completed_task(self, task):
+        if task in self.tasks:
+            self.tasks.remove(task)
+            self.completed.append(task)
+        
